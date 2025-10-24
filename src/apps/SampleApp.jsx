af643a9b441a676e3e329a55cfc460111d51cc11
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 15:00:29 -0500

    Update SampleApp.jsx

--- src/apps/SampleApp.jsx
+++ src/apps/SampleApp.jsx
@@ -1,1 +1,13 @@
+// src/apps/SampleApp.jsx
+import React from "react";
 
+const SampleApp = () => {
+  return (
+    <div style={{ padding: "20px", color: "white" }}>
+      <h2>Sample App</h2>
+      <p>This is a placeholder app for Nathen OS.</p>
+    </div>
+  );
+};
+
+export default SampleApp;
