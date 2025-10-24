d5878889c0e26d0dc87470354c6578b30ae1bf98
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 14:30:24 -0500

    Update Window.jsx

--- Src/Components/Window.jsx
+++ Src/Components/Window.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+const Window = ({ children, title, onClose }) => {
+  return (
+    <div className="window">
+      <div className="window-titlebar">
+        <span>{title}</span>
+        <button onClick={onClose}>✖</button>
+      </div>
+      <div className="window-content">{children}</div>
+    </div>
+  );
+};
+
+export default Window;
