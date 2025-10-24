7de4bdfbde8faff043dae09e3455f2e5ab87f760
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 15:22:26 -0500

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,14 +1,18 @@
-// src/utils/storage.js
 import localforage from "localforage";
 
 const APP_KEY = "nathen_apps";
 
+// Save a single app
 export const saveApp = async (app) => {
   const apps = (await localforage.getItem(APP_KEY)) || [];
   apps.push(app);
   await localforage.setItem(APP_KEY, apps);
 };
 
+// Load all apps (named export)
 export const loadApps = async () => {
   return (await localforage.getItem(APP_KEY)) || [];
 };
+
+// Alias for backwards compatibility with App.jsx
+export const loadData = loadApps;
