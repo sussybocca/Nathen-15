cd0b84adf385ee138102f700b5368f1256ba5761
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 14:29:16 -0500

    Update LoadingScreen.jsx

--- Src/Components/LoadingScreen.jsx
+++ Src/Components/LoadingScreen.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import { motion } from "framer-motion";
 
+const LoadingScreen = () => (
+  <motion.div
+    className="loading-screen"
+    initial={{ opacity: 1 }}
+    animate={{ opacity: 0 }}
+    transition={{ duration: 2 }}
+  >
+    <h1>Nathen is Loading...</h1>
+  </motion.div>
+);
+
+export default LoadingScreen;
