28fe57ce3b4e7aba9246712a1bdbedb618503ab3
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 14:29:59 -0500

    Update Taskbar.jsx

--- Src/Components/Taskbar.jsx
+++ Src/Components/Taskbar.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import DevConsole from "../apps/DevConsole";
+import SampleApp from "../apps/SampleApp";
+import EmojiLogoBuilder from "./EmojiLogoBuilder";
 
+const Taskbar = ({ openApp }) => {
+  return (
+    <div className="taskbar">
+      <button onClick={() => openApp(DevConsole, "Dev Console")}>
+        🖥 Dev Console
+      </button>
+      <button onClick={() => openApp(SampleApp, "Sample App")}>
+        📦 Sample App
+      </button>
+      <button onClick={() => openApp(EmojiLogoBuilder, "Emoji Logo Builder")}>
+        🎨 Emoji Builder
+      </button>
+    </div>
+  );
+};
+
+export default Taskbar;
